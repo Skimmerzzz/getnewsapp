ab1fa927b4caecea7ca4f0b6017cafdf3f0f3a0f
Skimmerzzz <skimmer@mail.ru>
2015-05-13 19:59:21 +0300

    Stupid Csv file writer write all rows

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ def main():
     end_date = datetime.date(2015, 4, 20)
     cr = newscrawler.ArchiveCrawlerBezformataRu()
     # TODO Make a dictionary may be
-    news = cr.get_region_news('45', start_date, end_date)
+    #news = cr.get_region_news('45', start_date, end_date)
+    news = cr.get_region_news('45')
+    wr = newscrawler.FileWriter()
+    wr.write_news_list(news)
 
-    #print(news)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
